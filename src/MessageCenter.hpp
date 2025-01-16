10a4f6f03f1d2f7df5c31d2806621a1f86d2f751
Volkan Orhan <contactme@volkanorhan.com>
2025-01-16 19:09:07 +0300

    MessageCenter arrangements

--- src/MessageCenter.hpp
+++ src/MessageCenter.hpp
@@ -15,9 +15,11 @@ You should have received a copy of the GNU General Public License along with thi
 #include <QMainWindow>
 #include <APIStructures.hpp>
 #include <PlayListDTOs.hpp>
+#include <Interfaces/PlayerSignals.hpp>
 
 namespace ModPlugPlayer {
-    class MessageCenter : public QObject
+    class MessageCenter : public QObject,
+                          public Interfaces::PlayerSignals
     {
         Q_OBJECT
     public:
@@ -30,53 +32,58 @@ namespace ModPlugPlayer {
     signals:
         //Player
         //Request Signals
-        void openRequested(const std::filesystem::path filePath);
-        void stopRequested();
-        void stopRequested(const PlayListItem playListItem);
-        void playRequested();
-        void playRequested(const PlayListItem playListItem);
-        void pauseRequested();
-        void pauseRequested(const PlayListItem playListItem);
-        void resumeRequested();
-        void resumeRequested(const PlayListItem playListItem);
-        void previousRequested();
-        void nextRequested();
-        void volumeChangeRequested(const int volume);
-        void timeScrubbingRequested(const int position);
-        void repeatModeChangeRequested(const ModPlugPlayer::RepeatMode repeatMode);
-        void eqStateChangeRequested(const bool activated);
-        void dspStateChangeRequested(const bool activated);
-        void alwaysOnTopStateChangeRequested(const bool alwaysOnTop);
-        void titleBarHidingStateChangeRequested(const bool hide);
-        void snappingToViewPortStateChangeRequested(const bool toBeSnappedToViewPort);
-        void keepingStayingInViewPortStateChangeRequested(const bool toBeKeptStayingInViewPort);
+        void openRequested(const std::filesystem::path filePath) override;
+        void stopRequested() override;
+        void stopRequested(const PlayListItem playListItem) override;
+        void playRequested() override;
+        void playRequested(const PlayListItem playListItem) override;
+        void pauseRequested() override;
+        void pauseRequested(const PlayListItem playListItem) override;
+        void resumeRequested() override;
+        void resumeRequested(const PlayListItem playListItem) override;
+        void previousRequested() override;
+        void nextRequested() override;
+        void volumeChangeRequested(const int volume) override;
+        void timeScrubbingRequested(const int position) override;
+        void repeatModeChangeRequested(const ModPlugPlayer::RepeatMode repeatMode) override;
+        void eqStateChangeRequested(const bool activated) override;
+        void dspStateChangeRequested(const bool activated) override;
+        void alwaysOnTopStateChangeRequested(const bool alwaysOnTop) override;
+        void titleBarHidingStateChangeRequested(const bool hide) override;
+        void snappingToViewPortStateChangeRequested(const bool toBeSnappedToViewPort) override;
+        void keepingStayingInViewPortStateChangeRequested(const bool toBeKeptStayingInViewPort) override;
+        void amigaFilterChangeRequested(const AmigaFilter amigaFilter) override;
+        void interpolationFilterChangeRequested(const InterpolationFilter interpolationFilter) override;
 
         //Response Signals
-        void loaded(const std::filesystem::path filePath, bool successfull);
-        void loaded(const PlayListItem playListItem, bool successfull);
-        void stopped();
-        void stopped(const PlayListItem playListItem);
-        void playingStarted(SongFileInfo songFileInfo);
-        void playingStarted(const PlayListItem playListItem);
-        void paused();
-        void paused(const PlayListItem playListItem);
-        void resumed();
-        void resumed(const PlayListItem playListItem);
-        void previous();
-        void next();
-        void volumeChanged(const int volume);
-        void timeScrubbed(const int position);
-        void repeatModeChanged(const RepeatMode repeat);
-        void eqStateChanged(const bool activated);
-        void dspStateChanged(const bool activated);
-        void alwaysOnTopStateChanged(const bool alwaysOnTop);
-        void titleBarHidingStateChanged(const bool hide);
-        void snappingToViewPortStateChanged(const bool snapToViewPort);
-        void keepingStayingInViewPortStateChanged(const bool toBeKeptStayingInViewPort);
-
-        //Song signals
-        void elapsedTimeChanged(const int seconds);
-        void trackDurationChanged(const size_t songDurationSeconds);
-        void trackTitleChanged(const QString songTitle);
+        void loaded(const std::filesystem::path filePath, bool successfull) override;
+        void loaded(const PlayListItem playListItem, bool successfull) override;
+        void stopped() override;
+        void stopped(const PlayListItem playListItem) override;
+        void playingStarted() override;
+        void playingStarted(const SongFileInfo songFileInfo) override;
+        void playingStarted(const PlayListItem playListItem) override;
+        void paused() override;
+        void paused(const SongFileInfo songFileInfo) override;
+        void paused(const PlayListItem playListItem) override;
+        void resumed() override;
+        void resumed(const SongFileInfo songFileInfo) override;
+        void resumed(const PlayListItem playListItem) override;
+        void previous() override;
+        void next() override;
+        void volumeChanged(const int volume) override;
+        void timeScrubbed(const int position) override;
+        void repeatModeChanged(const RepeatMode repeat) override;
+        void eqStateChanged(const bool activated) override;
+        void dspStateChanged(const bool activated) override;
+        void alwaysOnTopStateChanged(const bool alwaysOnTop) override;
+        void titleBarHidingStateChanged(const bool hide) override;
+        void snappingToViewPortStateChanged(const bool snapToViewPort) override;
+        void keepingStayingInViewPortStateChanged(const bool toBeKeptStayingInViewPort) override;
+        void elapsedTimeChanged(const int seconds) override;
+        void trackDurationChanged(const size_t songDurationSeconds) override;
+        void trackTitleChanged(const QString songTitle) override;
+        void amigaFilterChanged(const AmigaFilter amigaFilter) override;
+        void interpolationFilterChanged(const InterpolationFilter interpolationFilter) override;
     };
 }
