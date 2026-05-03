50ffef68a3ad98bb360d406d78a09da16409feaa
Volkan Orhan <contactme@volkanorhan.com>
2026-05-03 18:11:43 +0300

    Include mutex for MessageCenter thread safety

--- src/MessageCenter.hpp
+++ src/MessageCenter.hpp
@@ -16,6 +16,7 @@ You should have received a copy of the GNU General Public License along with thi
 #include <APIStructures.hpp>
 #include <PlayListDTOs.hpp>
 #include <Interfaces/PlayerSignals.hpp>
+#include <mutex>
 
 namespace ModPlugPlayer {
     class MessageCenter : public QObject,
